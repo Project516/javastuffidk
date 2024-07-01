5179838a6fda18fd228f0ce61687392bb763fcb7
Project516 <138796702+Project516@users.noreply.github.com>
2024-07-01 18:20:49 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(0001);
+		System.out.println(9 + 10) ;
+		System.out.println(2 * 9) ;
+		String name = "BOB" ;
+		System.out.println(name) ;
+  }
+}
