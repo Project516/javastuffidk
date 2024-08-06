e9928c1e548cbd9beb3070503c4d670b36bba407
Project516 <138796702+Project516@users.noreply.github.com>
2024-08-06 11:37:17 -0500

    n

---
+++ areafinder.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+	public class areafinder {
+		public static void main(String [] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("this is a area finder of a square!");
+		System.out.println("what is the length?");
+		String length = scanner.nextLine();
+		System.out.print("the length is " + length + " correct?");
+	}
+}
+		
